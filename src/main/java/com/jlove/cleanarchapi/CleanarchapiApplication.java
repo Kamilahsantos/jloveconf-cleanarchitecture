df014a7eac44b7fdb6bf76a6c7895db122eb2cdc
Kamila fatima santos oliveira <kamila.fatima@amedigital.com>
2020-12-04 22:27:03 -0300

    base da aplicação

---
+++ src/main/java/com/jlove/cleanarchapi/CleanarchapiApplication.java
@@ -0,0 +1,13 @@
+package com.jlove.cleanarchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchapiApplication.class, args);
+	}
+
+}
